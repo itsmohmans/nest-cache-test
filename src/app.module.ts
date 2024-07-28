0f24a1da8e3c691ca8e531b5900f4c4feee8aeeb
Mohamed Mansour <mohmans@pm.me>
2024-07-28 21:01:16 +0300

    add cache config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: 'localhost', // process.env.REDIS_HOST ??
+            port: 6379, // parseInt(process.env.REDIS_PORT) ??
+          },
+        }),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  async getHello(): Promise<string> {
+    await this.cacheManager.set(
+      'hello',
+      'world',
+      Number(process.env.VERIFICATION_TOKEN_TTL) || 86400000, // ttl in ms
+    );
+    return await this.cacheManager.get('hello');
+    // return 'Hello World!';
   }
 }
